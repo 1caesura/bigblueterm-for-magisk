2539f32e11cc5f5cec5ad0ece74803813fe25861
1caesura <mahirrsturnn@gmail.com>
2026-09-23 18:36:08 +0600

    fixed a small typo

--- customize.sh
+++ customize.sh
@@ -4,8 +4,8 @@ PROPFILE=false
 POSTFITYPE=false
 LATESTARTSERVICE=false
 
-# prnt custom text to magisk during flashing
-ui_print "- installing the fonts.."
+# print custom text to magisk during flashing
+ui_print "λ installing the fonts.."
 
 # enforce system permissions for fonts
 set_perm_recursive $MODPATH/system/fonts 0 0 0755 0644
